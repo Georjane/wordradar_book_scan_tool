84c9a93737240dd06b4d6197e21f80e299fe0286
Georjane <pzgeorjane@gmail.com>
2021-02-22 11:11:19 +0100

    Display words and their coreesponding parial match

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 
-const info = [];
-const string = ['genuine', 'yesooo', 'one', 'elaborate', '32', 'yes'];
+const dictionary = [];
+const string = ['genuine', 'yesooo', 'one', 'elaborate', 'yes', 'abandoning', 'elaborated'];
 const wordsNotFound = [];
 
 fs.createReadStream('words.csv')
   .pipe(csv())
   .on('data', function (row) {
-    info.push(row.word);
+    dictionary.push(row.word);
   })
   .on('end', function () {
-    const numberOfWordsFound = containsWord(string, info);
+    const numberOfWordsFound = containsWord(string, dictionary);
     const percentageMatch = calculatePercentage(string, numberOfWordsFound)
-  console.log(percentageMatch);
-  console.log(wordsNotFound);
+    console.log('>>>>>>>>>>>>>> Percentage Match is ' + percentageMatch );
+    partlyMatchedWords(wordsNotFound, dictionary)
   });
 
 const containsWord = (words, dictionary) => {
@@ -33,4 +33,20 @@ const calculatePercentage = (words, numberOfWordsFound) => {
   return (numberOfWordsFound/words.length)*100
 }
 
+const partlyMatchedWords = (words, dictionary) => {
+  let hashOfWords = {};
+  words.forEach(parentWord => {
+    dictionary.forEach(childWord => {
+      let regexChildWord = new RegExp(escapeRegExp(childWord));
+      if (regexChildWord.test(parentWord)) {
+        hashOfWords[regexChildWord] = parentWord
+      }
+      return hashOfWords
+    });
+  });
+  console.log(hashOfWords);
+}
 
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
