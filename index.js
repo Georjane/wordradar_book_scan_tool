22b39d218aaf24aefff28eb36cf2ce7fa8bd14e8
Georjane <pzgeorjane@gmail.com>
2021-02-22 10:06:37 +0100

    Display percentage match and words not found

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const fs = require('fs');
 const csv = require('csv-parser');
-// const { info } = require('console');
+
 const info = [];
-fs.createReadStream('input.csv')
+const string = ['genuine', 'yesooo', 'one', 'elaborate', '32', 'yes'];
+const wordsNotFound = [];
+
+fs.createReadStream('words.csv')
   .pipe(csv())
   .on('data', function (row) {
-    info.push(row.Firstname);
+    info.push(row.word);
   })
   .on('end', function () {
-    console.log(info);
+    const numberOfWordsFound = containsWord(string, info);
+    const percentageMatch = calculatePercentage(string, numberOfWordsFound)
+  console.log(percentageMatch);
+  console.log(wordsNotFound);
   });
+
+const containsWord = (words, dictionary) => {
+  let numberOfWordsFound = 0;
+  words.forEach(word => {
+    if (dictionary.includes(word)) {
+      numberOfWordsFound += 1
+    } else {
+      wordsNotFound.push(word)
+    }
+  });
+  return numberOfWordsFound
+}
+
+const calculatePercentage = (words, numberOfWordsFound) => {
+  return (numberOfWordsFound/words.length)*100
+}
+
+
